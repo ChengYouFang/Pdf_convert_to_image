e3b1b7d3e611abb7262c7cfc6a5cf6f1e4c8fed9
ChengYou Fang <s47548081@hotmail.com>
2013-02-24 01:47:35 +0800

    Pdf convert to image.

---
+++ PDFConvertImage/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PDFConvertImage
+{
+    partial class PDFtoImage
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_LoadFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_LoadFile
+            // 
+            this.button_LoadFile.Location = new System.Drawing.Point(12, 11);
+            this.button_LoadFile.Name = "button_LoadFile";
+            this.button_LoadFile.Size = new System.Drawing.Size(75, 23);
+            this.button_LoadFile.TabIndex = 0;
+            this.button_LoadFile.Text = "載入檔案";
+            this.button_LoadFile.UseVisualStyleBackColor = true;
+            this.button_LoadFile.Click += new System.EventHandler(this.button_LoadFile_Click);
+            // 
+            // PDFtoImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(116, 43);
+            this.Controls.Add(this.button_LoadFile);
+            this.Name = "PDFtoImage";
+            this.Text = "PDF Convert to Image";
+            this.Load += new System.EventHandler(this.PDFtoImage_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_LoadFile;
+    }
+}
+
---
+++ PDFConvertImage/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PDFConvertImage
+{
+    public partial class PDFtoImage : Form
+    {
+        public PDFtoImage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_LoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = getDeskPath();
+            dialog.Filter = "PDF|*.pdf";
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                //倍數
+                short pixelZoom = 10;
+
+                //http://www.cnblogs.com/kongxianghai/archive/2012/04/09/2438321.html
+                //頁
+                Acrobat.CAcroPDPage pdfPage = null;
+
+                //頁面大小
+                Acrobat.CAcroPoint pdfPoint = null;
+
+                //矩形
+                Acrobat.CAcroRect pdfRect = null;
+
+                //http://forums.codeguru.com/showthread.php?31751-What-do-I-need-for-CreateObject(-AcroExch.PDDoc-)
+                Acrobat.CAcroPDDoc pdfDoc = (Acrobat.CAcroPDDoc)Microsoft.VisualBasic.Interaction.CreateObject("AcroExch.PDDoc", "");
+                if (pdfDoc.Open(dialog.FileName))
+                {
+                    //PDF頁數
+                    int pageCount = pdfDoc.GetNumPages();
+                    for (int i = 0; i < pageCount; i++)
+                    {
+                        //取得目前頁數
+                        pdfPage = (Acrobat.CAcroPDPage)pdfDoc.AcquirePage(i);
+
+                        //取得頁面大小
+                        pdfPoint = (Acrobat.CAcroPoint)pdfPage.GetSize();
+
+                        //http://forums.adobe.com/thread/305835
+                        pdfRect = (Acrobat.CAcroRect)Microsoft.VisualBasic.Interaction.CreateObject("AcroExch.Rect", "");
+
+                        //左邊
+                        pdfRect.Left = 0;
+
+                        //頂端
+                        pdfRect.Top = 0;
+
+                        //右邊
+                        pdfRect.right = (short)(pdfPoint.x * pixelZoom);
+
+                        //底部
+                        pdfRect.bottom = (short)(pdfPoint.y * pixelZoom);
+
+                        //取得整個頁面
+                        pdfPage.CopyToClipboard(pdfRect, (short)(pdfRect.Left * pixelZoom), (short)(pdfRect.Top * pixelZoom), (short)(100 * pixelZoom));
+
+                        ///<summary>http://msdn.microsoft.com/en-us/library/system.windows.forms.idataobject.aspx
+                        ///You retrieve stored data from an IDataObject by calling the GetData method and specifying the data format in the format parameter. Set the  autoConvert parameter to false to retrieve only data that was stored in the specified format. To convert the stored data to the specified format, set autoConvert to true, or do not use autoConvert.
+                        ///</summary>
+                        IDataObject loClipboardData = Clipboard.GetDataObject();
+
+                        //Get Image
+                        Bitmap pdfBitmap = (Bitmap)loClipboardData.GetData(DataFormats.Bitmap);
+
+                        //Save Image
+                        pdfBitmap.Save(getDeskPath() + (i + 1) + ".jpeg", ImageFormat.Jpeg);
+                    }
+                    pdfDoc.Close();
+
+                    ///<summary>http://msdn.microsoft.com/en-us/library/system.runtime.interopservices.marshal.releasecomobject.aspx
+                    ///This method is used to explicitly control the lifetime of a COM object used from managed code. You should use this method to free the underlying COM object that holds references to resources in a timely manner or when objects must be freed in a specific order.
+                    ///</summary>
+                    Marshal.ReleaseComObject(pdfPage);
+                    Marshal.ReleaseComObject(pdfRect);
+                    Marshal.ReleaseComObject(pdfDoc);
+                    MessageBox.Show("轉換成功");
+                }
+                else
+                {
+                    MessageBox.Show("檔案可能損毀了");
+                }
+            }
+        }
+
+        //取得桌面路徑
+        private static string getDeskPath()
+        {
+            return System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\";
+        }
+
+        private void PDFtoImage_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+    }
+}
